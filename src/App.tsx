bc7422e129d825f3cdd5faa376ed1fa2cc825803
Josh Marom <josh@josh.co.il>
2023-01-10 16:18:02 +0200

    Changes to be committed: Added container for a nicer layout Changed html button to a MUI button

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import { useMetricsGenerator } from "./hooks/useMetricsGenerator"
 import { DataTable } from "./components/widgets/DataTable"
 import Grid from "@mui/material/Unstable_Grid2"
 import Paper from "@mui/material/Paper"
+import Container from "@mui/material/Container"
+import Button from "@mui/material/Button"
 import { MetricsChart } from "./components/widgets/Charts"
 import { ConversionsChart } from "./components/widgets/Charts/ConversionsChart"
-import { Box } from "@mui/system"
+
 //import './App.css'
 
 function App() {
@@ -18,26 +20,30 @@ function App() {
 
   return (
     <div className="App">
-      <Grid container spacing={2} sx={{ p: 2, backgroundColor: "grey.200" }}>
-        <Grid xs={12}>
-          <button onClick={() => generateMetrics()}>Generate Metrics</button>
+      <Container maxWidth="lg" sx={{ p: 2, backgroundColor: "grey.200" }}>
+        <Grid container spacing={2}>
+          <Grid xs={12}>
+            <Button variant="contained" onClick={() => generateMetrics()}>
+              Generate Metrics
+            </Button>
+          </Grid>
+          <Grid sm={8}>
+            <Paper sx={{ p: 2 }}>
+              <DataTable rows={metrics} />
+            </Paper>
+          </Grid>
+          <Grid sm={4}>
+            <Paper sx={{ p: 2 }}>
+              <ConversionsChart metrics={metrics} />
+            </Paper>
+          </Grid>
+          <Grid xs={12}>
+            <Paper sx={{ p: 2 }}>
+              <MetricsChart metrics={metrics} />
+            </Paper>
+          </Grid>
         </Grid>
-        <Grid sm={8}>
-          <Paper sx={{ p: 2 }}>
-            <DataTable rows={metrics} />
-          </Paper>
-        </Grid>
-        <Grid sm={4}>
-          <Paper sx={{ p: 2 }}>
-            <ConversionsChart metrics={metrics} />
-          </Paper>
-        </Grid>
-        <Grid xs={12}>
-          <Paper sx={{ p: 2 }}>
-            <MetricsChart metrics={metrics} />
-          </Paper>
-        </Grid>
-      </Grid>
+      </Container>
     </div>
   )
 }
